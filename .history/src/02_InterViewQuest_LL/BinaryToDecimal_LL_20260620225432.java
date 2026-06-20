17929b9ac5f3c7e64c2d38b02cf91417328f6d8f
Vedant Dighe <vedantdighe2707@gmail.com>
2026-06-20 23:09:00 +0530

    Convert Binary to decimal digits

---
+++ .history/src/02_InterViewQuest_LL/BinaryToDecimal_LL_20260620225432.java
@@ -0,0 +1,1 @@
+public class BinaryToDecimal_LL{}
---
+++ .history/src/02_InterViewQuest_LL/BinaryToDecimal_LL_20260620225611.java
@@ -0,0 +1,63 @@
+public class BinaryToDecimal_LL {
+
+    private Node head;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public BinaryToDecimal_LL(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+    }
+    
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    
+    public int binaryToDecimal(){
+        
+        int decimalValue = 0;
+    }
+
+}
+
+
